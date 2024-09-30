6cdaf4838af6a7af702993627b9aae61d3f4fa4b
Saket Nerurkar <saketanerurkar@gmail.com>
2024-09-30 20:59:46 +0530

    code to convert rgb888 to rgb 565

--- training.py
+++ training.py
@@ -12,6 +12,13 @@ EPOCHS = 20
 NUM_CLASSES = 7
 TRAIN_DIR = 'Dataset/Train'
 
+def rgb_to_rgb565(image):
+    r = (image[:, :, 0] >> 3) & 0x1F
+    g = (image[:, :, 1] >> 2) & 0x3F
+    b = (image[:, :, 2] >> 3) & 0x1F
+    rgb565 = (r << 11) | (g << 5) | b
+    return rgb565.astype(np.uint16)
+
 # Data generators for training and validation
 train_datagen = ImageDataGenerator(rescale=1.0/255.0, validation_split=0.2)
 train_generator = train_datagen.flow_from_directory(
@@ -31,6 +38,12 @@ validation_generator = train_datagen.flow_from_directory(
     color_mode='rgb',  # Ensures the model uses RGB888
     subset='validation'
 )
+
+for images, labels in train_generator:
+    images_rgb565 = rgb_to_rgb565(images)
+
+for images, labels in validation_generator:
+    images_rgb565 = rgb_to_rgb565(images)
 
 # Build the MobileNet model
 base_model = MobileNet(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3), include_top=False, weights='imagenet')
